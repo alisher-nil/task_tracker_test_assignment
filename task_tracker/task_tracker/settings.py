645d8c4e97b722b8a2a0ef18eb8214e2b6ec0595
Alisher Nildibayev <alisher.nil@gmail.com>
2025-05-15 20:03:47 +0500

    Implement Task model with fields and initial migration; add TITLE_FIELD_MAX_LENGTH constant

--- task_tracker/task_tracker/settings.py
+++ task_tracker/task_tracker/settings.py
@@ -10,7 +10,6 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -19,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tasks",
 ]
 
 MIDDLEWARE = [
@@ -50,20 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "task_tracker.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -80,10 +72,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -92,13 +80,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- task_tracker/tasks/models.py
+++ task_tracker/tasks/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from task_tracker.constants import TITLE_FIELD_MAX_LENGTH
+
+User = get_user_model()
+
+
+class Task(models.Model):
+    """
+    Model representing a task in the task tracker application.
+    Each task has a title, description, creation date, update date,
+    completion status, and an owner (user).
+    """
+
+    title = models.CharField(max_length=TITLE_FIELD_MAX_LENGTH)
+    description = models.TextField(blank=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False, db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.title
