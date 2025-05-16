ac9ed3914a3c36ca48bc451bc87e80e2d48d3464
Alisher Nildibayev <alisher.nil@gmail.com>
2025-05-16 14:50:36 +0500

    Remove unused imports and clean up authentication URL patterns

--- task_tracker/api/urls.py
+++ task_tracker/api/urls.py
@@ -1,21 +1,13 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# from rest_framework.routers import DefaultRouter
-from .views import UserSignUpView
+from api.views import UserSignUpView
 
 app_name = "api"
-
-# auth_router = DefaultRouter()
-# auth_router.register("", AuthView, basename="register")
 
 auth_urlpatterns = [
     path("register/", UserSignUpView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 urlpatterns = [path("auth/", include(auth_urlpatterns))]
