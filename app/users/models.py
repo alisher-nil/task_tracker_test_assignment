97c47d7c4a0f5339bc8907fc1a039296e88c3bc0
Alisher Nildibayev <alisher.nil@gmail.com>
2025-05-18 00:02:27 +0500

    Update REQUIRED_FIELDS in CustomUser model to include username and password

--- app/users/models.py
+++ app/users/models.py
@@ -9,4 +9,4 @@ class CustomUser(AbstractUser):
 
     email = models.EmailField("email address", unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username", "password"]
