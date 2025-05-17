070c5328540af439c90836bd56a2e36cd6320ec3
Alisher Nildibayev <alisher.nil@gmail.com>
2025-05-18 00:02:21 +0500

    Add admin configurations for Task and CustomUser models

--- app/tasks/admin.py
+++ app/tasks/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Admin interface for the Task model."""
+
+    list_display = ("title", "owner", "created_at", "updated_at", "completed")
+    list_filter = ("completed", "created_at", "updated_at", "owner")
+    search_fields = ("title", "description", "owner__username")
+    date_hierarchy = "created_at"
+
+    fieldsets = (
+        (None, {"fields": ("title", "description")}),
+        ("Status", {"fields": ("completed",)}),
+        ("Ownership", {"fields": ("owner",)}),
+        (
+            "Timestamps",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    readonly_fields = ("created_at", "updated_at")
+
+    def get_queryset(self, request):
+        """Optimize query by prefetching related owner."""
+        return super().get_queryset(request).select_related("owner")
--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Admin configuration for the custom user model."""
+
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("username", "email", "first_name", "last_name")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
