7a7a62f1e1f2d32cb7c0f2a82ac76f5113fab594
Alisher Nildibayev <alisher.nil@gmail.com>
2025-05-17 18:23:29 +0500

    Fix formatting inconsistencies in ASGI, WSGI, and UsersConfig files

--- app/api/views.py
+++ app/api/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, views, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+
 from tasks.models import Task
 
 from .serializers import SignUpSerializer, TasksSerializer
